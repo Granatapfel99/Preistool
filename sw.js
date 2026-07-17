7065bbeeccb46b536ac05835e6ce602c217205f8
Granatapfel99 <cdietz36@googlemail.com>
2026-07-17 02:51:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Netzausfall startet. Live-Daten (Supabase) werden NIE gecacht.
    HTML wird "network-first" geladen -> neue Versionen landen automatisch,
    ohne dass man die App manuell neu installieren muss. */
-const CACHE = 'thein-v22';
+const CACHE = 'thein-v23';
 const SHELL = ['thein.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
